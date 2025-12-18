a9f71003b47e1df37d9e8da1ca1e923635149a05
ahoop004 <ahoop004@odu.edu>
2025-12-17 21:37:54 -0500

    deletes

--- src/f110x/policies/random_policy.py
+++
@@ -1,3 +0,0 @@
-
-def random_policy(action_space, obs):
-    return action_space.sample()
--- src/f110x/policies/simple_heuristic.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-def simple_heuristic(action_space,obs):
-    # Example: small forward velocity, no steer
-    return np.array([0.0, 1.0], dtype=np.float32)
